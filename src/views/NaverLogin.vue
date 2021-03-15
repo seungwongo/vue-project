e8a5d9ddf474761d761fa69e62e068fae62562c0
Seungwon Go <seungwon.go@gmail.com>
2021-03-15 20:13:05 +0900

    update

--- src/views/NaverLogin.vue
+++ src/views/NaverLogin.vue
@@ -1,49 +1,57 @@
 <template>
-  <div id="naverIdLogin"></div>
+  <div>
+    <div id="naverIdLogin"></div>
+    <button type="button" @click="logout">로그아웃</button>
+  </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      naverLogin:null,
+      naverLogin: null,
     };
   },
   mounted() {
-    this.naverLogin = new window.naver.LoginWithNaverId(
-      {
-        clientId: "EOJ3XgstraTAtMOprSi6", //개발자센터에 등록한 ClientID
-        callbackUrl: "http://localhost:8080/naverlogin", //개발자센터에 등록한 callback Url
-        isPopup: false, //팝업을 통한 연동처리 여부
-        loginButton: {color: "green", type: 3, height: 60} //로그인 버튼의 타입을 지정
-      }
-    );
-    
+    this.naverLogin = new window.naver.LoginWithNaverId({
+      clientId: "zFcLWPMTcDQTNTB6iIOy", //개발자센터에 등록한 ClientID
+      callbackUrl: "http://localhost:8080/naverlogin", //개발자센터에 등록한 callback Url
+      isPopup: false, //팝업을 통한 연동처리 여부
+      loginButton: { color: "green", type: 3, height: 60 }, //로그인 버튼의 타입을 지정
+    });
+
     //설정정보를 초기화하고 연동을 준비
     this.naverLogin.init();
 
     this.naverLogin.getLoginStatus((status) => {
-				if (status) {
-          console.log(status)
-          console.log(this.naverLogin.user)
-          
-					//필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크
-					var email = this.naverLogin.user.getEmail();
-					if( email == undefined || email == null) {
-						alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
-						//사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함
-						this.naverLogin.reprompt();
-						return;
-					}
-				} else {
-					console.log("callback 처리에 실패하였습니다.");
-				}
-			});
-    
+      if (status) {
+        console.log(status);
+        console.log(this.naverLogin.user);
+
+        //필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크
+        var email = this.naverLogin.user.getEmail();
+        if (email == undefined || email == null) {
+          alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
+          //사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함
+          this.naverLogin.reprompt();
+          return;
+        }
+      } else {
+        console.log("callback 처리에 실패하였습니다.");
+      }
+    });
   },
   methods: {
     logout() {
-      this.naverLgoin.logout();
-    }
-  }
-}
+      const accessToken = this.naverLogin.accessToken.accessToken;
+      const url = `/oauth2.0/token?grant_type=delete&client_id=zFcLWPMTcDQTNTB6iIOy&client_secret=bUW7FZMpS9&access_token=${accessToken}&service_provider=NAVER`;
+
+      axios.get(url).then((res) => {
+        console.log(res.data);
+      });
+
+      //https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=zFcLWPMTcDQTNTB6iIOy&client_secret=bUW7FZMpS9&access_token=AAAAOOCeX4fAa_NxKPAmJW8C1UeLxGT3nM0wRV33irhyHyRua1JJrfrp0jZwfbOD0r502Id9mbhb0YiA9_NvCXGAwws&service_provider=NAVER
+    },
+  },
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
-const target = 'http://127.0.0.1:3000'; //proxy 요청을 보낼 서버 주소
-
 module.exports = {
   chainWebpack: config => {
     config.plugins.delete('prefetch'); //prefetch 삭제
   },
   devServer: {
-    port: 8080,
     proxy: {
-      //proxy 요청을 보낼 api 시작 부분     
-      '^/api': {
-        target,
-        changeOrigin: true
+      '/oauth2.0': {
+        target: 'https://nid.naver.com'
       }
     }
   }
