12c77f03dec30121e42ae94b76979f50cabb0880
Seungwon Go <seungwon.go@gmail.com>
2021-03-16 18:07:16 +0900

    Update KakaoLogin.vue

--- src/views/KakaoLogin.vue
+++ src/views/KakaoLogin.vue
@@ -1,99 +1,71 @@
 <template>
-  <div >
+  <div>
     <a id="custom-login-btn" @click="kakaoLogin()">
       <img
         src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
         width="222"
       />
     </a>
-    </div>
+    <button type="button" @click="kakaoLogout">카카오 로그아웃</button>
+  </div>
 </template>
 <script>
 // import axios from 'axios'
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
-      code:"",
+      code: "",
     };
   },
   mounted() {
     // Kakao.init('c2875030d590c5acc4f04aa15597bab4');
     // Kakao.isInitialized();
-    this.kakaoLogout();
+    // this.kakaoLogout();
   },
   methods: {
     kakaoLogin() {
-        window.Kakao.Auth.login({
-            scope : 'profile, account_email',
-            success: this.getKakaoAccount,
-        });
+      window.Kakao.Auth.login({
+        scope: "profile, account_email",
+        success: this.getKakaoAccount,
+      });
     },
-    getKakaoAccount(){
-        window.Kakao.API.request({
-            url:'/v2/user/me',
-            success : res => {
-                const kakao_account = res.kakao_account;
-                const nickname = kakao_account.profile.nickname; //카카오 닉네임
-                const email = kakao_account.email //카카오 이메일
+    getKakaoAccount() {
+      window.Kakao.API.request({
+        url: "/v2/user/me",
+        success: (res) => {
+          const kakao_account = res.kakao_account;
+          const nickname = kakao_account.profile.nickname; //카카오 닉네임
+          const email = kakao_account.email; //카카오 이메일
 
-                console.log('nickname', nickname);
-                console.log('email', email);
-                //로그인 처리 구현
+          console.log("nickname", nickname);
+          console.log("email", email);
+          //로그인 처리 구현
 
+          console.log(kakao_account);
+          this.$store.commit("user", kakao_account);
 
-                console.log(kakao_account);
-                // this.login(kakao_account);
-                
-                // const userInfo = {
-                //     nickname : kakao_account.profile.nickname,
-                //     email : kakao_account.email,
-                //     password : '',
-                //     account_type : 2,
-                // }
-
-                  // axios.post(`http://localhost:8080/account/kakao`,{
-                  //     email : userInfo.email,
-                  //     nickname : userInfo.nickname
-                  // })
-                  // .then(res => {
-                  //   console.log(res);
-                  //   console.log("데이터베이스에 회원 정보가 있음!");
-                  // })
-                  // .catch(err => {
-                  //     console.log(err);
-                  //   console.log("데이터베이스에 회원 정보가 없음!");
-                  // })
-                // console.log(userInfo);
-                alert("로그인 성공!");
-            },
-            fail : error => {
-                // this.$router.push("/errorPage");
-                console.log(error);
-            }
-        })
+          alert("로그인 성공!");
+        },
+        fail: (error) => {
+          // this.$router.push("/errorPage");
+          console.log(error);
+        },
+      });
     },
-    kakaoLogout(){
-     window.Kakao.Auth.logout((response) => {
-       //로그아웃
-       console.log(response);
-     });
-   }
+    kakaoLogout() {
+      if (!window.Kakao.Auth.getAccessToken()) {
+        console.log("Not logged in.");
+        return;
+      }
 
-    // async login(kakao_account) {
-    //   await this.$api("/api/login", {
-    //     param:[
-    //       {email:kakao_account.email, nickname:kakao_account.profile.nickname, gender:kakao_account.gender},
-    //       {nickname:kakao_account.profile.nickname, gender:kakao_account.gender}
-    //       ]
-    //   });
-
-    //   this.$store.commit("user",kakao_account);
-      
-    //   this.$router.push({ path: 'test'});
-    // }
-  }
-}
+      window.Kakao.Auth.logout((response) => {
+        //로그아웃
+        console.log("access token:", window.Kakao.Auth.getAccessToken());
+        console.log("log out:", response);
+      });
+    },
+  },
+};
 </script>
